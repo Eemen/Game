fe00f2f7a0d56456c743ba10349876158e55a333
Eemen <egemenkeskin61@gmail.com>
2023-11-29 08:27:09 +0100

    update
    update

--- Games/src/at/eee/games/firstgame/Rectangles.java
+++ Games/src/at/eee/games/firstgame/Rectangles.java
@@ -4,9 +4,6 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
-    private int x;
-    private int y;
-
 
     public Rectangles(String title) {
         super(title);
@@ -14,28 +11,16 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.x = 100;
 
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        System.out.println(delta);
-        this.x += delta/2;
-        //this.y++;
 
-        if (this.x>600){
-            this.x = 0;
-        }
-        if (this.y>600){
-            this.y = 0;
-        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.x,this.y,100,100);
-        graphics.drawString("Hello World!",50,50);
     }
 
     public static void main(String[] argv) {
